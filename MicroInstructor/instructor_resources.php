112bdd428b15f2805f60ade5f55569a4c0bc04d0
Humberto Barreto <hbarreto@depauw.edu>
2025-07-09 10:17:55 -0700

    update IR

--- MicroInstructor/instructor_resources.php
+++ MicroInstructor/instructor_resources.php
@@ -1,33 +1,31 @@
 <?
-if ($_SESSION["business_instructor"] === "") {
-	header('Location: http://www.depauw.edu/learn/econexcel/busanalytics/instructor/index.htm');
+if ($_SESSION["microExcel_instructor"] === "") {
+	header('Location: https://barretoh.github.io/GitHubmicro/MicroInstructor/index.htm');
 }
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
         "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
-<title>Instructor Login</title>
-<meta name="keywords" content="business, analytics, business analytics, Excel, DePauw, DePauw University, Humberto Barreto, Economics, Economics Department, Economics Department DePauw, business school">
-<meta name="description" content="Business Analytics with Excel maintained by Humberto Barreto at DePauw University">
-<link href="../../css/ba.css" rel="stylesheet" type="text/css">
-<link href="../css/ba.css" rel="stylesheet" type="text/css">
+<title>Intermediate Microeconomics</title>
+<link href="../css/micro.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 
 <div id="sitemast">
-  <h1><font color="#333333">Business Analytics with Excel</font></h1>
+  <h1><font color="#333333">Intermediate Microeconomics with Excel</font></h1>
 </div>
 <div id="sitenav">
-<h4><font color="#000000">Open Access</font></h4>
-<a href="http://www.depauw.edu/learn/econexcel/busanalytics/book/index.htm">Book</a>
-<a href="http://www.depauw.edu/learn/econexcel/busanalytics/exceladdins/index.htm">Excel Add-ins</a>
-<a href="http://www.depauw.edu/learn/econexcel/busanalytics/excelworkbooks/index.htm">Excel Workbooks</a>
-<a href="http://www.depauw.edu/learn/econexcel/busanalytics/instructor/index.htm">Instructor Resources</a>
-</div>
+<h4><font color="#000000">Menu</font></h4>
 
-
+<a href="https://barretoh.github.io/GitHubmicro/MicroBook/CSWiz.htm">COMPARATIVE STATICS WIZARD</a>
+<a href="https://barretoh.github.io/GitHubmicro/MicroBook/excelworkbooks.htm">EXCEL WORKBOOKS</a>
+<a href="https://barretoh.github.io/GitHubmicro/MicroInstructor/index.htm">INSTRUCTOR RESOURCES</a>  
+<a href="https://barretoh.github.io/GitHubmicro/Workshop/index.htm">WORKSHOP</a>
+<a href="https://barretoh.github.io/GitHubmicro/videos.htm">VIDEOS</a>
+<a href="https://barretoh.github.io/GitHubmicro/index.htm">SITE HOME</a></div><!-- This </div> closes the div at the beginning of this menu, <div id="sitenav"> -->
 <div id="main">
+
 <h3>Instructor Resources</h3>
   <p>The materials here are designed for teachers of Introductory Economics (or similar) 
     courses.<p>
